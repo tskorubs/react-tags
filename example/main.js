941033bf31d3129d50dff016373d1c0758c8d946
Mafa14 <m.silva.gambardelo@gmail.com>
2022-06-23 15:59:36 -0300

    Updated Readme and updated code to react 18

--- example/main.js
+++ example/main.js
@@ -292,4 +292,5 @@ const App = () => {
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<App />);
